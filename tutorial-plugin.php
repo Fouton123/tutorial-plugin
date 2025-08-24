3cbe48eb6c6633077b38799a248e9a7874a6b433
Eric Kausch <erickausch@comcast.net>
2025-08-24 10:46:12 -0400

    Init

---
+++ tutorial-plugin.php
@@ -0,0 +1,15 @@
+/*
+ * Plugin Name:       Tutorial Plugin
+ * Plugin URI:        
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Eric Kausch
+ * Author URI:        https://github.com/Fouton123
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       tutorial-plugin
+ * Domain Path:       /languages
+ */
